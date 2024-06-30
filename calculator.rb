39b6c0fd6e5e42fb6afd7025503d286f1b256fac
Sumit Bansal <sbansal.mait@gmail.com>
2024-06-30 17:49:14 +0530

    Update Calculator add method to handle string with custom delimiters.

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,12 @@
 class Calculator
   def add(numbers)
-    numbers.split(/\n|,/).reduce(0) do |sum, val|
+    delimiter = ','
+    if numbers.start_with?('//')
+      delimiter_line, numbers = numbers.split("\n", 2)
+      delimiter = delimiter_line[2..-1]
+    end
+
+    numbers.split(/[\n#{delimiter}]/).reduce(0) do |sum, val|
       val.to_i + sum
     end
   end
